f95826b1873309ce308645554b4115bacbf4365e
Rodrigo <tazzsoni@gmail.com>
2020-12-28 20:57:52 -0300

    Organizações

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Reports from './pages/Reports';
 import Products from './pages/Products';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path='/' exact component={Home} />
+          <Route path='/' exact component={Login} />
+          <Route path='/home' exact component={Home} />
           <Route path='/reports' component={Reports} />
           <Route path='/products' component={Products} />
         </Switch>
