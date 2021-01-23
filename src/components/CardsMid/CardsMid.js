a88dcfd7ca36d0c1a48926535121cf5147e42796
Rodrigo <tazzsoni@gmail.com>
2021-01-23 08:15:44 -0300

    css navbar

--- src/components/CardsMid/CardsMid.js
+++ src/components/CardsMid/CardsMid.js
@@ -3,7 +3,6 @@ import { Card } from 'react-bootstrap'
 import {
     PieChart, Pie, Cell, Label
 } from 'recharts';
-import './CardsMid.css'
 
 
 function CardsMid() {
--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -13,22 +13,70 @@ function Navbar({ nome }) {
 
   const logout = () => localStorage.clear()
 
+  const styles = {
+    navbar: {
+      backgroundColor: '#17A5B8',
+      height: "55",
+      display: 'flex',
+      alignItems: 'center',
+    },
+    headerApp: {
+      fontFamily: "'Lato', sans-serif",
+      color: 'white',
+    },
+    menuBars: {
+      marginLeft: '2rem',
+      fontSize: '2rem',
+      background: 'none',
+    },
+    navMenu: {
+      zIndex: 1,
+      backgroundColor: '#17A2B8',
+      width: 250,
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      position: 'fixed',
+      top: 0,
+      left: '-100%',
+      transition: '850ms',
+    },
+    navMenuActive: {
+      left: 0,
+      transition: '350ms',
+    },
+    navMenuItems: {
+      width: '100%',
+    },
+    navbarToggle: {
+      backgroundColor: '#17A2B8',
+      width: '100%',
+      height: 80,
+      display: 'flex',
+      justifyContent: 'flex-start',
+      alignItems: 'center',
+    }
+  }
+
   return (
     <>
       <IconContext.Provider value={{ color: '#fff' }}>
-        <div className='navbar'>
-          <Link to='#' className='menu-bars'>
+        <div style={styles.navbar} className='navbar'>
+          <Link to='#' style={styles.menuBars}>
             <FaIcons.FaBars onClick={showSidebar} />
           </Link>
-          <h3 className="headerApp">Olá {nome}, este é seu gerenciador finaceiro</h3>
-          <Link to='#' className='menu-bars'>
+          <h3 style={styles.headerApp}>Olá {nome}, este é seu gerenciador finaceiro</h3>
+          <Link to='#' style={styles.menuBars}>
             <FaIcons.FaSignInAlt onClick={logout} />
           </Link>
         </div>
+        {
+          //style={sidebar ? styles.navMenuActive : styles.navMenu}
+        }
         <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-          <ul className='nav-menu-items' onClick={showSidebar}>
-            <li className='navbar-toggle'>
-              <Link to='#' className='menu-bars'>
+          <ul style={styles.navMenuItems} onClick={showSidebar}>
+            <li style={styles.navbarToggle}>
+              <Link to='#' style={styles.menuBars}>
                 <AiIcons.AiOutlineClose />
               </Link>
             </li>
